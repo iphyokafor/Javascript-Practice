9ccd2cba135ad4894d83193e1e24b9e3f71dd0a6
Okafor Ifeoma Sandra <sandraifeoma22@gmail.com>
2021-11-06 23:59:26 +0100

    modal window logic to open and close modal

---
+++ modal-window/script.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = () => {
+    console.log('button clicked');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+};
+
+const closeModal = () => {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+    btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
+
+// Keyboard events(global events)
+//keydown, keyup, keypress
+document.addEventListener('keydown', e => {
+    console.log(e.key);
+
+    if (e.key === 'Escape') console.log('esc pressed');
+});
